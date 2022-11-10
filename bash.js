51e59ba5db361867ad32be289d2adcd5001ee7cb
Jamison Pereira <jamison.pereira@gmail.com>
2022-11-10 20:41:37 +0100

    ls module

--- bash.js
+++ bash.js
@@ -1,10 +1,14 @@
 process.stdout.write('prompt > ');
 
 const pwd = require('./pwd');
+const ls = require('./ls');
 
 process.stdin.on('data', (data) => {
   const cmd = data.toString().trim(); //remove the newline
   if (cmd === 'pwd') {
     process.stdout.write(pwd());
+  }
+  if (cmd === 'ls') {
+    ls();
   }
 });
